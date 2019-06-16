a0e932d771d861c071605bab750377ac25eb253b
Joel Stephen <jojostev@gmail.com>
2019-06-16 11:20:58 +0400

    # hashed the random_int
    Was put there for checking, now obselete

--- random_joke.py
+++ random_joke.py
@@ -2,7 +2,7 @@ import requests
 import random
 
 random_int = random.randint(0, 2164)
-print(random_int)
+# print(random_int)
 url = 'https://xkcd.com/'+str(random_int)+'/info.0.json'
 
 resp = requests.get(url=url)
