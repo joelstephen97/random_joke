0abb4e45fe6f37192802031f3f7af662679e4d42
Joel Stephen <jojostev@gmail.com>
2019-06-16 11:18:04 +0400

    Created random_joke.py
    Need to make the file into a function, so on calling function in other file, one can use it.

---
+++ random_joke.py
@@ -0,0 +1,10 @@
+import requests
+import random
+
+random_int = random.randint(0, 2164)
+print(random_int)
+url = 'https://xkcd.com/'+str(random_int)+'/info.0.json'
+
+resp = requests.get(url=url)
+data = resp.json()
+print(data["img"])
